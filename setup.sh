2975254be2a45345390b4d0ee8fd09e99f048c97
Gavroche <arthur.reppelin@gmail.com>
2022-09-20 13:34:12 +0200

    setup

--- setup.sh
+++ setup.sh
@@ -1,40 +1,31 @@
 #!/bin/bash
 
-success() {
-	echo -e "$(tput setaf 2)$1$(tput sgr0)"
-}
-
 inform() {
 	/usr/bin/echo -e "$(tput setaf 6)$1$(tput sgr0)"
 }
 
-warning() {
-	echo -e "$(tput setaf 1)$1$(tput sgr0)"
-}
-
-
 inform "Creating src folder"
-/usr/bin/mkdir /root/src
+/usr/bin/mkdir /src
 
 # Download Frontend
 inform "Downloading Frontend Software"
-/usr/bin/mkdir /root/src/Frontend
-/usr/bin/git clone https://github.com/G4vr0ch3/Frontend /root/src/Frontend
-/usr/bin/chmod +x /root/src/Frontend/setup.sh
+/usr/bin/mkdir /src/Frontend
+/usr/bin/git clone https://github.com/G4vr0ch3/Frontend /src/Frontend
+/usr/bin/chmod +x /src/Frontend/setup.sh
 
 # Download Backend
 inform "Downloading Backend Software"
-/usr/bin/mkdir /root/src/Backend
-/usr/bin/git clone https://github.com/G4vr0ch3/Backend /root/src/Backend
-/usr/bin/chmod +x /root/src/Backend/setup.sh
+/usr/bin/mkdir /src/Backend
+/usr/bin/git clone https://github.com/G4vr0ch3/Backend /src/Backend
+/usr/bin/chmod +x /src/Backend/setup.sh
 
 # Installing Frontend
 inform "Installing Frontend Software"
-/root/src/Frontend/setup.sh
+/src/Frontend/setup.sh
 
 # Installing Backend
 inform "Installing Backend Software"
-/root/src/Frontend/setup.sh
+/src/Frontend/setup.sh
 
 # Creating shared folder
 inform "Creating docker shared volume"
