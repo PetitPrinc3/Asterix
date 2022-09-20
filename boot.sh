0361a8683245de325cb78f3f7b6e457c6b56db1c
Gavroche <arthur.reppelin@gmail.com>
2022-09-20 14:45:42 +0200

    ...

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 # Runing Docker containers
-/usr/bin/sudo -u docker_runner /usr/bin/docker run --privileged -v /dev:/dev -v InputFiles:/mnt/InputFiles --name frontend -d -it frontend
-/usr/bin/sudo -u docker_runner /usr/bin/docker run --privileged -v /dev:/dev -v InputFiles:/mnt/InputFiles --name backend -d -it backend
+/usr/bin/su - docker_runner "/usr/bin/docker run --privileged -v /dev:/dev -v InputFiles:/mnt/InputFiles --name frontend -d -it frontend"
+/usr/bin/su - docker_runner "/usr/bin/docker run --privileged -v /dev:/dev -v InputFiles:/mnt/InputFiles --name backend -d -it backend"
--- setup.sh
+++ setup.sh
@@ -10,13 +10,15 @@ inform "Creating src folder"
 # Download Frontend
 inform "Downloading Frontend Software"
 /usr/bin/git clone https://github.com/G4vr0ch3/Frontend /src/Frontend
-/usr/bin/chmod -R a=r /src/Frontend
+/usr/bin/chown -R docker_runner:docker /src/Frontend
+/usr/bin/chmod -R u=r /src/Frontend
 /usr/bin/chmod +x /src/Frontend/setup.sh
 
 # Download Backend
 inform "Downloading Backend Software"
 /usr/bin/git clone https://github.com/G4vr0ch3/Backend /src/Backend
-/usr/bin/chmod -R a=r /src/Backend
+/usr/bin/chown -R docker_runner:docker /src/Backend
+/usr/bin/chmod -R u=r /src/Backend
 /usr/bin/chmod +x /src/Backend/setup.sh
 
 # Installing Frontend
