9d3e0cbd5292836a6bcc48967a5722bf2b35c67a
Gavroche <arthur.reppelin@gmail.com>
2022-09-20 14:49:16 +0200

    ...

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Input device
-/usr/bin/su - docker_runner "/usr/bin/docker exec -w /usr/share/USBInputDetection -t frontend python3 main.py"
+/usr/bin/docker exec -w /usr/share/USBInputDetection -t frontend python3 main.py
