a2fb2889ed900a0e3f495e318a287c74e1e417d1
Gavroche <arthur.reppelin@gmail.com>
2022-09-28 20:09:02 +0200

    ...

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ def get_docker():
 
 
 def cmd_run(cmd):
-    if subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).returncode != 0: fail('Process failed. This is critical.'); print(cmd); fail("Exiting now."); exit()
+    if subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).wait() != 0: fail('Process failed. This is critical.'); print(cmd); fail("Exiting now."); exit()
+
 
 if subprocess.run('docker version', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stderr.decode('utf-8').strip() == '/bin/sh: 1: docker: not found': warning('Oops, docker is not installed. Installing now !'); get_docker()
 
@@ -80,20 +81,20 @@ success("UDEV rules added.")
 
 
 info("Building Frontend container...")
-cmd_run('/usr/bin/su - docker_runner -c "/usr/bin/docker build -t frontend /src/Frontend"', shell=True)
+cmd_run('/usr/bin/su - docker_runner -c "/usr/bin/docker build -t frontend /src/Frontend"')
 success("Frontend software installed.")
 
 info("Building Backend container...")
-cmd_run('/usr/bin/su - docker_runner -c "/usr/bin/docker build -t backend /src/Backend"', shell=True)
+cmd_run('/usr/bin/su - docker_runner -c "/usr/bin/docker build -t backend /src/Backend"')
 success("Backend software installed.")
 
 
 with spinner('Creating relevant docker volumes...'):
-    cmd_run('/usr/bin/su - docker_runner -c "/usr/bin/docker volume create --name InputFiles"', shell=True)
+    cmd_run('/usr/bin/su - docker_runner -c "/usr/bin/docker volume create --name InputFiles"')
     info('InputFiles volume created')
-    cmd_run('/usr/bin/su - docker_runner -c "/usr/bin/docker volume create --name OutputFiles"', shell=True)
+    cmd_run('/usr/bin/su - docker_runner -c "/usr/bin/docker volume create --name OutputFiles"')
     info('OutputFiles volume created')
-    cmd_run('/usr/bin/su - docker_runner -c "/usr/bin/docker volume create --name SharedDB"', shell=True)
+    cmd_run('/usr/bin/su - docker_runner -c "/usr/bin/docker volume create --name SharedDB"')
     info('SharedDB volume created')
 success("All relevant docker volumes created.")
 
@@ -107,7 +108,7 @@ success("USB UIDs database initialized.")
 with spinner('Starting containers...'):
     cmd_run("/usr/bin/cp boot.sh /opt/docker_runner/boot.sh")
     cmd_run("/usr/bin/cp boot.sh /opt/docker_runner/run.sh")
-    cmd_run('/usr/bin/su - docker_runner -c "/bin/bash /opt/docker_runner/boot.sh"', shell=True)
+    cmd_run('/usr/bin/su - docker_runner -c "/bin/bash /opt/docker_runner/boot.sh"')
 success("Docker containers started.")
 
 
@@ -122,22 +123,22 @@ success('64Gb System disk image was initialized.')
 
 
 with spinner('Collecting VirtIO drivers...'):
-    cmd_run("/usr/bin/curl https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.221-1/virtio-win-0.1.221.iso -o /src/win10-VM/virtio_drivers.iso", shell=True)
+    cmd_run("/usr/bin/curl https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.221-1/virtio-win-0.1.221.iso -o /src/win10-VM/virtio_drivers.iso")
 success('VirtIO drivers collected.')
 
 with spinner('Collecting windows UUID...'):
-    UUID=cmd_run("""/usr/bin/wget --no-check-certificate -qO- "https://uupdump.net/known.php?q=windows+10+21h2+arm64" | grep 'href="\./selectlang\.php?id=.*"' -o | sed 's/^.*id=//g' | sed 's/"$//g' | head -n1""", shell=True)
+    UUID=cmd_run("""/usr/bin/wget --no-check-certificate -qO- "https://uupdump.net/known.php?q=windows+10+21h2+arm64" | grep 'href="\./selectlang\.php?id=.*"' -o | sed 's/^.*id=//g' | sed 's/"$//g' | head -n1""")
     WIN_LANG="en-us"
 success('Collected windows UUID.')
 
 with spinner('Collected windows downloader from UUPdump...'):
-    cmd_run('/usr/bin/mkdir tmp', shell=True)
-    cmd_run(f'/usr/bin/wget --no-check-certificate -O "tmp/uupdump.zip" "https://uupdump.net/get.php?id={UUID}&pack={WIN_LANG}&edition=professional&autodl=2"', shell=True)
-    cmd_run('/usr/bin/unzip -q "tmp/uupdump.zip"', shell=True)
+    cmd_run('/usr/bin/mkdir tmp')
+    cmd_run(f'/usr/bin/wget --no-check-certificate -O "tmp/uupdump.zip" "https://uupdump.net/get.php?id={UUID}&pack={WIN_LANG}&edition=professional&autodl=2"')
+    cmd_run('/usr/bin/unzip -q "tmp/uupdump.zip"')
 success('Collected windows downloader.')
 
 info('Downloading windows ISO. This will take some time.')
-cmd_run("tmp/uup_download_linux.sh", shell=True)
+cmd_run("tmp/uup_download_linux.sh")
 success('Windows ISO collected.')
 
 
@@ -145,7 +146,7 @@ with spinner('Finalizing VM environment...'):
     cmd_run("/usr/bin/mv tmp/*.ISO /src/win10_VM")
 success("VM environment finalized")
 
-cmd_run("/usr/bin/rm -r tmp", shell=True)
+cmd_run("/usr/bin/rm -r tmp")
 
 info("""When you are ready to setup the VM, execute "bash vm_setup.sh". Follow the instructions provided on the repository's README.""")
 
