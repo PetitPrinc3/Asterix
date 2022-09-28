693eb9d7584ef55339979e2b811a8b32e8748a4a
Gavroche <arthur.reppelin@gmail.com>
2022-09-28 19:29:53 +0200

    ...

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 # Runing Docker containers
-/usr/bin/docker run --privileged -v /dev/Frontend:/dev:ro -v InputFiles:/mnt/InputFiles --name frontend -d -it frontend
+/usr/bin/docker run --privileged -v /root:/dev/Frontend:ro -v InputFiles:/mnt/InputFiles --name frontend -d -it frontend
 /usr/bin/docker run --privileged -v /dev/Backend:/dev:ro -v OutputFiles:/mnt/OutputFiles:ro -v SharedDB:/mnt/SharedDB:ro --name backend -d -it backend
