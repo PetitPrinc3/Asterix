7d5e5cee07b3f44a9e3f7159762a8d2419ce221e
Gavroche <arthur.reppelin@gmail.com>
2022-10-04 16:04:57 +0200

    Asterix_

--- Host/Asterix.py
+++ Host/Asterix.py
@@ -14,15 +14,18 @@ create()
 subprocess.run('cp USB_ID.db /var/lib/docker/volumes/DataShare/_data/', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 subprocess.run("su - docker_runner -c '/usr/bin/docker exec -w /usr/share/USBHandler -it frontend python3 main.py'", shell = True)
-info('You can now remove the USB input drive.')
 subprocess.run("umount /var/lib/docker/volumes/USBInputDevice/_data/USBInputPart", shell = True, stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
 subprocess.run("umount /var/lib/docker/volumes/USBInputDevice/_data/USBInputDisk", shell = True, stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
+info('You can now remove the USB input drive.')
+
+subprocess.run('cp -r /var/lib/docker/volumes/InputFiles/_data/* /var/lib/docker/volumes/OutputFiles/_data/*', shell = True, stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
+subprocess.run('cp -r /var/lib/docker/volumes/DataShare/_data/user_inp.json /var/lib/docker/volumes/DataShare/_data/trt_result.json', shell = True, stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
 
 
 subprocess.run("su - docker_runner -c '/usr/bin/docker exec -w /usr/share/USBHandler -it backend python3 main.py'", shell = True)
-info('You can now remove the USB output drive.')
 subprocess.run("/var/dev/USBOutputPart", shell = True, stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
 subprocess.run("umount /dev/USBOutputDisk", shell = True, stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
+info('You can now remove the USB output drive.')
 
 
 with spinner('Waiting for drive removal...'):
