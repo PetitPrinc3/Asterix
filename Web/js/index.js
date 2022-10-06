52150aa108bcdde15ef57392505085f1a37ed6d2
Gavroche <arthur.reppelin@gmail.com>
2022-10-06 22:26:03 +0200

    Web Development

--- Web/js/index.js
+++ Web/js/index.js
@@ -7,9 +7,11 @@ $(".main_logo").hover(
     function () {
       $(".back-hexa img:last-child").css("opacity", "0");
       $(".back-hexa img:nth-child(1)").css("opacity", "1");
+      $(".descriptor b").css("color", "#fbea2c")
     },
     function () {
       $(".back-hexa img:last-child").css("opacity", "1");
       $(".back-hexa img:nth-child(1)").css("opacity", "0");
+      $(".descriptor b").css("color", "white")
     }
   );
