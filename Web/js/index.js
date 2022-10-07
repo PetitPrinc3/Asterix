1da7519ae78324d1f8c043b4af39f2441e2fb004
Gavroche <arthur.reppelin@gmail.com>
2022-10-07 18:50:35 +0200

    Web Dev

--- Web/js/index.js
+++ Web/js/index.js
@@ -15,3 +15,17 @@ $(".main_logo").hover(
       $(".descriptor b").css("color", "white")
     }
   );
+
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("menuItems");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
