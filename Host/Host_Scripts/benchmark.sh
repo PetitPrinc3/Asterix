a4dfeacad92d4fbc275eef15d623cb75d44f1b33
Gavroche <arthur.reppelin@gmail.com>
2022-10-26 15:35:00 +0200

    Benchmarking

--- Host/Host_Scripts/benchmark.sh
+++ Host/Host_Scripts/benchmark.sh
@@ -4,7 +4,7 @@ read -n 1 -s -r -p "Plug in USB Drives and press any key to begin."
 
 echo BENCHMARK STARTED AT 0 SECONDS
 SECONDS=0
-/usr/bin/python3 /opt/asterix/Asterix.py | <<EOF
+/usr/bin/python3 /opt/asterix/Asterix.py <<EOF
 0;1;2;3;4;5;6;7;8;9;10;11;12;13
 f
 Y
